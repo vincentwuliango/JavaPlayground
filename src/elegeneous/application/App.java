f8231f95895e343d5dfc6b6354b96ea6237107e6
Vincent <122006254+vincentwuliango@users.noreply.github.com>
2023-02-15 10:02:05 +0700

    Update App.java

--- src/elegeneous/application/App.java
+++ src/elegeneous/application/App.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class App {
 	public static void main(String[] args) {
-		testFizzBuzz();
+		isPalindromeTest();
 	}
 	
-	public static void FizzBuzz(Integer number) {
+	public static void fizzBuzz(Integer number) {
 		for (int i = 1; i <= number; i++) {
 			if ((i % 3 == 0) && (i % 5 == 0)) {
 				System.out.println("FizzBuzz");
@@ -27,6 +27,35 @@ public class App {
 		System.out.print("Input number: ");
 		number = scan.nextInt();
 		
-		FizzBuzz(number);
+		fizzBuzz(number);
+	}
+	
+	public static boolean isPalindrome(String word) {
+		for (int i = 0; i < word.length(); i++) {
+			if (word.charAt(i) != word.charAt(word.length() - i - 1)) {
+				return false;
+			}
+		}
+		return true;
+
+
+//		var temp = "";
+//		for (int i = word.length() - 1; i >= 0; i--) {
+//			temp += word.charAt(i);
+//		}
+//		var result = temp.equals(word) ? "Palindrome" : "Not Palindrome";
+//		System.out.printf("%s is %s\n", word, result);
+	}
+	
+	public static void isPalindromeTest() {
+		isPalindrome("aba");
+		isPalindrome("kodok");
+		isPalindrome("kodoks");
+		isPalindrome("abc");
+		isPalindrome("212");
+		isPalindrome("212212");
+		isPalindrome("ab");
+		isPalindrome("Vincent");
+		isPalindrome("Aba");
 	}
 }
