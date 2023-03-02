a5ca030eb15355fb9580518da960e1796aa172b9
Vincent <122006254+vincentwuliango@users.noreply.github.com>
2023-03-02 14:34:47 +0700

    Update Sort.java

--- src/elegeneous/interview/Sort.java
+++ src/elegeneous/interview/Sort.java
@@ -1,5 +1,29 @@
 package elegeneous.interview;
 
-public class Sort {
+import org.junit.jupiter.api.Test;
 
+public class Sort {
+	void bubbleSort(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			for (int j = 0; j < arr.length - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+				}
+			}
+		}
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+		
+	}
+	
+	
+	@Test
+	void bubbleSortTest() {
+		int[] arr = {5, 1, 4, 2, 8};
+		bubbleSort(arr);
+	}
 }
